8b0d1d9e22e09e6bde0c2a1631f3e6b1f66ee888
JorensM <jorensmerenjanu@gmail.com>
2024-02-09 12:49:47 +0200

    update vite config to build a 'dist_dev' folder if developmenet environment

--- new(vite)/vite.config.js
+++ new(vite)/vite.config.js
@@ -1,21 +1,28 @@
 import hotReloadExtension from 'hot-reload-extension-vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: './src/navkeys.ts',
-            output: {
-                assetFileNames: '[name].[ext]',
-                entryFileNames: '[name].js'
-            }
+
+
+export default defineConfig(( { mode }) => {
+    const outDir = mode == 'development' ? 'dist_dev' : 'dist';
+
+    return {
+        build: {
+            outDir,
+            rollupOptions: {
+                input: './src/navkeys.ts',
+                output: {
+                    assetFileNames: '[name].[ext]',
+                    entryFileNames: '[name].js'
+                }
+            },
+            minify: false
         },
-        minify: false
-    },
-    plugins: [
-        hotReloadExtension({
-        log: true,
-          backgroundPath: 'src/worker.ts' // src/pages/background/index.ts
-        })
-    ]
+        plugins: [
+            hotReloadExtension({
+            log: true,
+            backgroundPath: 'src/worker.ts' // src/pages/background/index.ts
+            })
+        ]
+    }
 });
